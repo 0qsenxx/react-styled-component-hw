1755fb2da6cd52aada5b8a941582cb6549f4e86d
0qsenxx <semen.antipyuk@gmail.com>
2024-07-23 22:12:59 +0300

    hw

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.div`
+  max-width: 1400px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  width: 100%;
+`;
